1155bc2762ef376c59f3a25c30a9ad23b12fced1
LhommeFrancais <54549266+LhommeFrancais@users.noreply.github.com>
2022-10-26 00:17:53 -0500

    Added dashboard telemetry to vision test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestVisionTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestVisionTele.java
@@ -1,14 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.Vision;
 
 @TeleOp
 public class TestVisionTele extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
         Robot robot = new Robot(gamepad1, gamepad2, hardwareMap);
         Vision vision = new Vision();
         vision.init(hardwareMap);
@@ -17,8 +23,8 @@ public class TestVisionTele extends LinearOpMode {
 
         while (opModeIsActive()) {
             robot.update();
-            telemetry.addData("Vision Output:", vision.getMatTotal());
-            telemetry.update();
+            dashboardTelemetry.addData("Vision Output:", vision.getMatTotal());
+            dashboardTelemetry.update();
         }
     }
 }
