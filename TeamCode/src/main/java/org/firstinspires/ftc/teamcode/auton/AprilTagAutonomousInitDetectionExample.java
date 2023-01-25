e6e3929747e1ad2e2afac20cd7c1624f6f78f17e
nathangendler <nathangendler06@gmail.com>
2023-01-24 23:25:05 -0800

    fixed fr this time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AprilTagAutonomousInitDetectionExample.java
@@ -41,6 +41,7 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode {
 
     static final double FEET_PER_METER = 3.28084;
 
+
     // Lens intrinsics
     // UNITS ARE PIXELS
     // NOTE: this calibration is for the C920 webcam at 800x448.
