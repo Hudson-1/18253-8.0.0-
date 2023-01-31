c81be01cc9ac0c4a1012ad6641482ea02d2d2db8
“misterjonny” <jaibel@pacbell.net>
2023-01-31 12:05:22 -0800

    Oops. Forgot to upload files.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionPoleRevised.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionPoleRevised.java
@@ -1,2 +1,245 @@
-package org.firstinspires.ftc.teamcode.subsystems;public class VisionPoleRevised {
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Config
+public class VisionPoleRevised implements Subsystem {
+
+    // Configurable Vision Variables
+    public static int webcamHeight = 240;
+    public static int webcamWidth = 320;
+    public static double FOV = 55.0;            // FOV of the webcam
+    public static double poleDiameter = 1.05;   // Actual size of the Pole's diameter in inches
+    public static double offset = 0.1;          // offset from pole surface to robot
+    public static double knownDistance = 12.0;  // Known distance to estimate the focal length
+    public static double knownImageWidth = 26.0; // pole width in the image
+    private double distanceFromPoleCenterToImageCenter;
+    private double widthOfTheClosestPole;
+    private int numberOfContours;
+
+    //backlog of frames to average out to reduce noise
+    ArrayList<double[]> frameList;
+    //these are public static to be tuned in dashboard
+    public static double strictLowS = 130;
+    public static double strictHighS = 200;
+    public static double hueMin = 16;  // was 20
+    public static double hueMax = 30; // was 32
+    public static double saturationMin = 70;  // was 70
+    public static double saturationMax = 255;
+    public static double valueMin = 80; // was 80
+    public static double valueMax = 255;
+
+    public VisionPoleRevised() {
+        frameList = new ArrayList<>();
+    }
+
+
+    OpenCvCamera webcam;
+    private VisionPipeline visionPipeline;
+
+    @Override
+    public void init(HardwareMap map) {
+
+        // Initialize new VisionPipeline instance
+        visionPipeline = new VisionPipeline();
+
+        // Create a new Webcam instance and get the visionPipeline
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(map.get(WebcamName.class, "Webcam1"));
+        webcam.setPipeline(visionPipeline);
+
+        // Open the camera and start processing the frames from the camera
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(webcamWidth, webcamHeight, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+            }
+        });
+
+        FtcDashboard.getInstance().startCameraStream(webcam, 0);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public Telemetry telemetry() {
+        return null;
+    }
+
+    //
+    // VISION PIPELINE
+    //
+    class VisionPipeline extends OpenCvPipeline {
+
+        private final Mat workingMatrix = new Mat();
+
+        @Override
+        public Mat processFrame(Mat input) {
+            input.copyTo(workingMatrix);
+
+            if (workingMatrix.empty()) { // If the frame is empty, just return
+                return input;
+            }
+            Mat mat = new Mat();
+
+            //mat turns into HSV value
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+            if (mat.empty()) {
+                return input;
+            }
+
+            // lenient bounds will filter out near yellow, this should filter out all near yellow things(tune this if needed)
+            Scalar lowHSV = new Scalar(hueMin, saturationMin, valueMin); // lenient lower bound HSV for yellow
+            Scalar highHSV = new Scalar(hueMax, saturationMax, valueMax); // lenient higher bound HSV for yellow
+
+            Mat thresh = new Mat();
+
+            // Get a black and white image of yellow objects
+            Core.inRange(mat, lowHSV, highHSV, thresh);
+
+            Mat masked = new Mat();
+            //color the white portion of thresh in with HSV from mat
+            //output into masked
+            Core.bitwise_and(mat, mat, masked, thresh);
+            //calculate average HSV values of the white thresh values
+            Scalar average = Core.mean(masked, thresh);
+
+            Mat scaledMask = new Mat();
+            //scale the average saturation to 150
+            masked.convertTo(scaledMask, -1, 150 / average.val[1], 0);
+
+            Mat scaledThresh = new Mat();
+            //you probably want to tune this
+            Scalar strictLowHSV = new Scalar(0, strictLowS, 0); //strict lower bound HSV for yellow
+            Scalar strictHighHSV = new Scalar(255, strictHighS, 255); //strict higher bound HSV for yellow
+            //apply strict HSV filter onto scaledMask to get rid of any yellow other than pole
+            Core.inRange(scaledMask, strictLowHSV, strictHighHSV, scaledThresh);
+
+            Mat finalMask = new Mat();
+            //color in scaledThresh with HSV, output into finalMask(only useful for showing result)(you can delete)
+            Core.bitwise_and(mat, mat, finalMask, scaledThresh);
+
+            Mat edges = new Mat();
+            //detect edges(only useful for showing result)(you can delete)
+            Imgproc.Canny(scaledThresh, edges, 100, 200);
+
+            //contours, apply post processing to information
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            //find contours, input scaledThresh because it has hard edges
+            Imgproc.findContours(scaledThresh, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+            int maxWidth = 0;
+            Rect maxRect = new Rect();
+
+            //list of frames to reduce inconsistency, not too many so that it is still real-time, change the number from 5 if you want
+            if (frameList.size() > 5) {
+                frameList.remove(0);
+            }
+
+            numberOfContours = contours.size();
+
+            // Enumerate each contour and find the one that's closest to the camera, assuming that is the closet pole
+            for (MatOfPoint c : contours) {
+                MatOfPoint2f contour = new MatOfPoint2f(c.toArray());
+                Rect boundingRect = Imgproc.boundingRect(contour);
+
+                // This finds the width of the contour
+                if (boundingRect.width > maxWidth) {
+                    maxWidth = boundingRect.width;
+                    maxRect = boundingRect;
+                }
+
+                c.release(); // releasing the buffer of the contour, since after use, it is no longer needed
+                contour.release(); // releasing the buffer of the copy of the contour, since after use, it is no longer needed
+            }
+
+            //release all the data
+            input.release();
+            scaledThresh.copyTo(input);
+            scaledThresh.release();
+            scaledMask.release();
+            mat.release();
+            masked.release();
+            edges.release();
+            thresh.release();
+            finalMask.release();
+
+            // change the return to whatever mat you want
+            // for example, if I want to look at the lenient thresh:
+            // return thresh;
+            // note that you must not do thresh.release() if you want to return thresh
+            // you also need to release the input if you return thresh(release as much as possible)
+            //return input;
+
+            // Save the width of the closest pole to widthOfTheClosestPole in pixels
+            widthOfTheClosestPole = maxWidth;
+
+            // Calculate the distance from the center of the pole to the center of the image
+            // If the value is negative, it is at the left side of the center;
+            // otherwise, it is at the right side of the image center
+            distanceFromPoleCenterToImageCenter = maxRect.x + (maxRect.width / 2.0) - (webcamWidth / 2.0);
+
+            return input;
+        }
+    }
+
+    private double getOffset() {
+        return (poleDiameter * 0.5 + offset);
+    }
+
+
+    public double getDistanceFromPoleCenterToImageCenter() {
+        return distanceFromPoleCenterToImageCenter;
+    }
+
+    public double getWidthOfTheClosestPole() {
+        return widthOfTheClosestPole;
+    }
+
+
+    public double getAngle() {
+        // This is supposed to be the angle between the line from camera to pole, and the line from camera to center of the image
+        // If this value is negative, it means the robot needs to turn left, if the value is positive, it means the robot needs to turn right
+        return distanceFromPoleCenterToImageCenter * FOV / webcamWidth;
+    }
+
+    public VisionPipeline getVisionPipeline() {
+        return visionPipeline;
+    }
+
+    private double getFocalLength(double measuredDistance, double realWidth, double imageWidth) {
+        return imageWidth * measuredDistance / realWidth;
+    }
+
+    public double getDistanceFromFocalLength() {
+        return (poleDiameter * getFocalLength(knownDistance, poleDiameter, knownImageWidth)) / widthOfTheClosestPole;
+    }
+
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/PoleAimTeleRevised.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/PoleAimTeleRevised.java
@@ -1,2 +1,154 @@
-package org.firstinspires.ftc.teamcode.tele;public class PoleAimTeleRevised {
+package org.firstinspires.ftc.teamcode.tele;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.VisionPole;
+import org.firstinspires.ftc.teamcode.subsystems.VisionPoleRevised;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Config
+@TeleOp
+public class PoleAimTeleRevised extends LinearOpMode {
+    boolean toggle = false;
+    boolean lastPress = false;
+    public static double angleRange = 5.0;     // smallest angle range that we can turn
+    public static double distanceRange = 3.0;  // smallest distance range that we can move
+    static int timer = 500;             // milliseconds that we sleep for
+
+    // DEFINES THE TWO STATES -- DRIVER CONTROL OR AUTO ALIGNMENT
+    public enum states {
+        DRIVER_CONTROL,
+        AUTO_ALIGN,
+    }
+
+    private states currentMode = states.DRIVER_CONTROL;
+
+    private boolean IsWithinAngleRange(double angle) {
+        if ((angle > (0-angleRange)) && (angle < angleRange)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean IsWithinDistanceRange(double distance) {
+        if ((distance > (0-distanceRange)) && (distance < distanceRange)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
+        Robot robot = new Robot(gamepad1, gamepad2, hardwareMap, false);
+
+        // Initialize custom cancelable SampleMecanumDrive class
+        SampleMecanumDrive drive = robot.getDriveClass().getDrive();
+
+        // We want to turn off velocity control for teleop
+        // Velocity control per wheel is not necessary outside of motion profiled auto
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        VisionPoleRevised visionPole = new VisionPoleRevised();
+        visionPole.init(hardwareMap);
+
+        while (opModeIsActive()) {
+            robot.update();
+            dashboardTelemetry.update();
+
+            switch (currentMode) {
+                case DRIVER_CONTROL:
+                    drive.setWeightedDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y,
+                                    -gamepad1.left_stick_x,
+                                    -gamepad1.right_stick_x
+                            )
+                    );
+
+                    // PRESSING BUTTON TRIGGERS THE AUTO-ALIGN ROUTINE
+                    boolean button = gamepad1.right_stick_button;
+
+                    if (button) {
+                        lastPress = true;
+                    }
+
+                    if (lastPress && !button) {
+                        toggle = !toggle;
+                        lastPress = false;
+                    }
+
+                    if (toggle) {
+                        currentMode = states.AUTO_ALIGN;
+
+                    } else {
+                        currentMode = states.DRIVER_CONTROL;
+                    }
+                    break;
+
+                case AUTO_ALIGN:
+
+                    boolean actions = false;
+
+                    // Get the angle that we need to turn
+                    double angle = 0 - visionPole.getAngle(); // we need to negate this value so the robot can understand
+
+                    if (!IsWithinAngleRange(angle)) {
+                        if (angle > 0) {
+                            telemetry.addData("turn left: ", angle);
+                        } else if (angle < 0) {
+                            telemetry.addData("turn right: ", -angle);
+                        }
+                        drive.turn(Math.toRadians(angle));
+                        actions = true;
+
+                    } else {
+                        telemetry.addData("angle falls within range: ", angle);
+
+                        // Get the distance between camera and pole using perceived focal length
+                        double distance = visionPole.getDistanceFromFocalLength();
+
+                        if (!IsWithinDistanceRange(distance)) {
+                            // Update the telemetry with the distance data
+                            telemetry.addData("distance we need to move forward: ", distance);
+
+                            Pose2d startPose = new Pose2d(0, 0, Math.toRadians(180)); // this is 180 bc bot is backwards
+                            drive.setPoseEstimate(startPose);
+                            TrajectorySequence Distance = drive.trajectorySequenceBuilder(startPose)
+                                    .forward(-distance) // note the negative to make it go forwards
+                                    .build();
+                            drive.followTrajectorySequence(Distance);
+
+                            actions = true;
+                        }
+
+                    }
+
+                    telemetry.update();
+                    lastPress = true;
+
+                    if (!actions) {     // If there are no actions being taken, we consider the job is done. Give back to the driver control mode.
+                        currentMode = states.DRIVER_CONTROL;
+                    } else {            // Otherwise, sleep to give other threads a chance to run
+                        sleep(timer);
+                    }
+
+                    break;
+            }
+        }
+    }
 }
